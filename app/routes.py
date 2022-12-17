2f277930de40cd7003e5b1939715ac0b10f208cc
Caleb Marshall <caleb.marshall108@gmail.com>
2022-12-17 18:15:45 -0500

    app/routes: update reference to appvar -> app

--- app/routes.py
+++ app/routes.py
@@ -2,26 +2,26 @@ import os
 
 from flask import render_template, redirect, url_for, flash, request
 
-from . import appvar
+from . import app
 
 
-@appvar.route('/')
-@appvar.route('/index')
+@app.route('/')
+@app.route('/index')
 def render_index_page():
     return render_template('index.html', is_index=True)
 
-@appvar.route('/about')
+@app.route('/about')
 def render_about_page():
     return render_template('about.html', is_about=True)
 
-@appvar.route('/services')
+@app.route('/services')
 def render_service_page():
     return render_template('services.html', is_services=True)
 
-@appvar.route('/portfolio')
+@app.route('/portfolio')
 def render_portfolio_page():
     return render_template('portfolio.html', is_portfolio=True)
 
-@appvar.route('/contact')
+@app.route('/contact')
 def render_contact_page():
     return render_template('contact.html', is_contact=True)
